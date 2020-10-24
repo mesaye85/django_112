bcc672e3fb8d8fa448a375ff63f99b3cfd06c44a
mesaye addisu <mesayeaddisu@mesayes-MacBook-Air.local>
2020-10-24 14:23:52 -0400

    New updates for Heroku deployment

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'yac#vqh!xz87b5#25@j07#jv2dslto%n@tb+1p6wodnt7j1nth'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1" ]
 
 
 # Application definition
